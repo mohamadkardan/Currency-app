64160a648769fd1343efb9c487970283d91fce1c
Mohammad Kardan <40921713+mohamadkardan@users.noreply.github.com>
2023-01-19 12:50:17 +0330

    remove a test comment from Constants.kt

--- app/src/main/java/com/example/currency/util/Constants.kt
+++ app/src/main/java/com/example/currency/util/Constants.kt
@@ -6,6 +6,4 @@ class Constants {
         val CONVERTOR_API_KEY = "d04063910eb3d027da85886e"
         val CONVERTOR_BASE_URL = "https://v6.exchangerate-api.com/v6/$CONVERTOR_API_KEY/"
     }
-
-// test comment
 }
